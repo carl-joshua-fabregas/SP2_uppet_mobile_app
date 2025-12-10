dec41c0a6cd08b72ac6d6d7ecb274757d4526ca6
carl-joshua-fabregas <cyfabregas@up.edu.ph>
2025-12-11 00:14:05 +0800

    Add: Frotend sign in:

--- frontend/uppet/app/(drawers)/_layout.jsx
+++ frontend/uppet/app/(drawers)/_layout.jsx
@@ -1,15 +1,22 @@
 import { Drawer } from "expo-router/drawer";
 
 export default function Menu() {
-  return (
-    <Drawer screenOptions={{ drawerPosition: "right" }}>
-      <Drawer.Screen
-        name="(tabs)"
-        options={{
-          title: "UPPET",
-          drawerLabel: "Name",
-        }}
-      ></Drawer.Screen>
-    </Drawer>
-  );
+	return (
+		<Drawer
+			screenOptions={{
+				drawerPosition: "right",
+				drawerStyle: {
+					width: "60%",
+				},
+			}}
+		>
+			<Drawer.Screen
+				name="(tabs)"
+				options={{
+					title: "UPPET",
+					drawerLabel: "Name",
+				}}
+			></Drawer.Screen>
+		</Drawer>
+	);
 }
--- frontend/uppet/app/login.jsx
+++ frontend/uppet/app/login.jsx
@@ -2,26 +2,50 @@ import { Text, View, StyleSheet } from "react-native";
 import { Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { DrawerActions, useNavigation } from "@react-navigation/native";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Index() {
-  const navigation = useNavigation();
-  return (
-    <View>
-      <Pressable
-        style={styles.myprofile}
-        onPress={() => navigation.navigate("(drawers)")}
-      >
-        <Ionicons name="grid-outline" size={50}></Ionicons>
-      </Pressable>
-    </View>
-  );
+	const navigation = useNavigation();
+
+	const [request, response, signIn] = Google.useAuthRequest({
+		expoClientId:
+			"6734110788-rn5ibmvbnn7tf00hmr0lihi6ph9ma1fs.apps.googleusercontent.com",
+		iosClientId:
+			"6734110788-9lbc61k9u8dg4tebk8uppo4ve75ju28b.apps.googleusercontent.com",
+		androidClientId:
+			"6734110788-dsgk74dm16ddm73bsuce679vcqif92pe.apps.googleusercontent.com",
+	});
+
+	useEffect(() => {
+		if (response?.type === "success") {
+			console.log(response.authentication);
+		}
+	}, [response]);
+
+	return (
+		<View>
+			<Pressable
+				style={styles.myprofile}
+				onPress={() => navigation.navigate("(drawers)")}
+			>
+				<Ionicons name="grid-outline" size={50}></Ionicons>
+			</Pressable>
+			{/* <Pressable onPress={signIn} title="Sign in with Google!">
+				<Text>Sign In</Text>
+			</Pressable> */}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  myprofile: {
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex",
-    margin: 0,
-  },
+	myprofile: {
+		justifyContent: "center",
+		alignItems: "center",
+		display: "flex",
+		margin: 0,
+	},
 });
